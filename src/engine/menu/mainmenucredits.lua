65cab1dbfe047546b88a54dc4f8743f04fabbf7f
BluBerry016 <50649185+unit0016@users.noreply.github.com>
2025-09-20 08:25:07 -0400

    oop

--- src/engine/menu/mainmenucredits.lua
+++ src/engine/menu/mainmenucredits.lua
@@ -116,7 +116,7 @@ function MainMenuCredits:init(menu)
                 "skarph",
                 "Diamond Deltahedron",
                 "ThePlasticPotato",
-                "",
+                "MihBoss",
                 "",
             },
             {
